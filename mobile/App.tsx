d71b47b21f3b76f4f9bbf58ae98a5e70cd12711d
Nei Barros <jlantonio86@gmail.com>
2023-01-18 10:27:14 -0300

    feat: add initial setup from mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,43 @@
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  useFonts
+} from '@expo-google-fonts/inter';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+
+import { Loading } from './src/components/Loading';
+
+export default function App() {
+  const [fontLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
+  if (!fontLoaded) {
+    return <Loading />
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello Word</Text>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#09090A',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontFamily: 'Inter_800ExtraBold',
+    color: '#fff'
+  }
+});
