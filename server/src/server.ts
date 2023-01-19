66944390d0c00e0f07ae17fd16e26b85d1f8beb2
Nei Barros <jlantonio86@gmail.com>
2023-01-19 09:46:10 -0300

    feat: add erd, libs, models and routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,11 @@
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
+import { AppRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-app.get("/", async (request, reply) => {
-  const habits = await prisma.habit.findMany();
-  return habits;
-});
+app.register(AppRoutes);
 
 app.listen({
   port: 3333,
