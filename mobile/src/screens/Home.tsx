f10f0219831067dbbd8ab5030d130409af29901c
Nei Barros <jlantonio86@gmail.com>
2023-01-20 14:09:50 -0300

    feat: add routes

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { ScrollView, Text, View } from "react-native";
 
 import { DAY_SIZE, HabitDay } from "../components/HabitDay";
@@ -10,6 +11,8 @@ const minimunSummarizedDatesSize = 18 * 5;
 const amountOfDatesToFill = minimunSummarizedDatesSize - datesFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -32,7 +35,10 @@ export function Home() {
       >
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date) => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => navigate('habit', { date: date.toISOString() })}
+            />
           ))}
           
           {amountOfDatesToFill > 0 && Array.from({ length: amountOfDatesToFill }).map((_, index) => (
